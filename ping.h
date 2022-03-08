6aa89a5b1769ec5ec968781d6069ffc23baefecd
Kush Jain <99990377+NegasonicX@users.noreply.github.com>
2022-03-08 10:36:02 +0530

    Add files via upload

---
+++ ping.h
@@ -0,0 +1,12 @@
+#ifndef __ping_h__
+#define __ping_h__
+
+#include "ping/ping_sock.h"
+
+void cmd_ping_on_ping_success(esp_ping_handle_t hdl, void *args);
+void cmd_ping_on_ping_timeout(esp_ping_handle_t hdl, void *args);
+void cmd_ping_on_ping_end(esp_ping_handle_t hdl, void *args);
+int cmd_ping_on_ping_results(int select);
+esp_err_t initialize_ping(uint32_t interval_ms, uint32_t task_prio, char * target_host, int ping_count);
+
+#endif /* __ping_h__ */
